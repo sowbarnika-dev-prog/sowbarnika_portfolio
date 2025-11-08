8e120258e78a98e4c78f1ad3dd7d335bc3ffd201
Sowbarnika-dev <>
2025-11-08 13:04:57 +0530

    file setuped

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/myportfolio/",
+  base: "/sowbarnika_portfolio/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
